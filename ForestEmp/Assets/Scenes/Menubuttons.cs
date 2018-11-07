286d176d5d85c01f94792abdb50eccb4012a2e66
t8laje00 <44665894+t8laje00@users.noreply.github.com>
2018-11-07 16:05:42 +0200

    Valikkoa ja testiä

---
+++ ForestEmp/Assets/Scenes/Menubuttons.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menubuttons : MonoBehaviour {
+
+    public GameObject MenuPanel;
+    public GameObject OptionsPanel;
+    public GameObject StartPanel;
+    public GameObject MainPanel;
+	// Use this for initialization
+	void Start ()
+    {
+        MainPanel.SetActive(true);
+        MenuPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+        StartPanel.SetActive(false);
+
+	}
+	
+    public void ShowMainPanel()
+    {
+        MainPanel.SetActive(true);
+        MainPanel.SetActive(true);
+        OptionsPanel.SetActive(false);
+        StartPanel.SetActive(false);
+    }
+
+    public void ShowOptionsPanel()
+    {
+        MainPanel.SetActive(true);
+        MainPanel.SetActive(false);
+        OptionsPanel.SetActive(true);
+        StartPanel.SetActive(false);
+    }
+
+    public void ShowStartPanel()
+    {
+        MainPanel.SetActive(true);
+        MainPanel.SetActive(false);
+        OptionsPanel.SetActive(false);
+        StartPanel.SetActive(true);
+    }
+
+    public void StartScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ ForestEmp/Assets/Scenes/Settings.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+    private Resolution[] Resolutions;
+    public Dropdown ResoDropdown;
+   // public Dropdown ScreenDropdown;
+    public bool Fullscreen;
+    public Toggle FullscreenToggle;
+    public Resolution resolution;
+    // Use this for initialization
+    void Start ()
+    {
+
+
+        //Fullscreen = false;
+
+
+        Resolutions = Screen.resolutions;
+        ResoDropdown.ClearOptions();
+        List<string> resolutionoptions = new List<string>();
+
+        
+       /* ScreenDropdown.ClearOptions();
+        List<string> screenoptions = new List<string>();
+        screenoptions.Add("Fullscreen");
+        screenoptions.Add("Windowed"); */
+
+
+        int currentresolutionindex = 0;
+
+
+
+
+
+
+
+
+        for (int i =0; i < Resolutions.Length; i++)
+        {
+            string resolutionoption = Resolutions[i].width + "x" + Resolutions[i].height;
+            resolutionoptions.Add(resolutionoption);
+
+            if(Resolutions[i].width==Screen.currentResolution.width && Resolutions[i].height==Screen.currentResolution.height)
+            {
+                currentresolutionindex = i;
+            }
+
+        }
+
+        ResoDropdown.AddOptions(resolutionoptions);
+
+        ResoDropdown.value = currentresolutionindex;
+        ResoDropdown.RefreshShownValue();
+
+
+	}
+
+
+    public void SetResolution(int resolutionindex)
+    {
+        resolution = Resolutions[resolutionindex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void FullscreenActive(bool value)
+    {
+        if (value == true)
+        {
+            Screen.fullScreen = true;
+        }
+        if (value == false)
+        {
+            Screen.fullScreen = false;
+        }
+    }
+
+
+
+
+    void Update ()
+    {
+		
+	}
+}
