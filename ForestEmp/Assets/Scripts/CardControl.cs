d2c345e9e0c57deb2440ab94eb75160bdcd76529
Tuuli Haapala <t6hatu01@students.oamk.fi>
2018-11-19 13:24:25 +0200

    GameUpdates

--- ForestEmp/Assets/Scripts/CardControl.cs
+++ ForestEmp/Assets/Scripts/CardControl.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class CardControl : MonoBehaviour {
 
-    public GameObject DemoCard;
+    public GameObject PinkCard;
+    public GameObject PurpleCard;
+    public GameObject RedCard;
+    public GameObject BlueCard;
+    public GameObject YellowCard;
+    public GameObject BrownCard;
+    public GameObject OrangeCard;
+    //public GameObject Buy;
 
     // Use this for initialization
     void Start()
     {
-        DemoCard.SetActive(false);
+        PinkCard.SetActive(false);
+        PurpleCard.SetActive(false);
+        RedCard.SetActive(false);
+        BlueCard.SetActive(false);
+        YellowCard.SetActive(false);
+        BrownCard.SetActive(false);
+        OrangeCard.SetActive(false);
+
     }
 
     // Update is called once per frame
@@ -22,8 +36,35 @@ public class CardControl : MonoBehaviour {
     {
         if (collision.gameObject.tag == "waypoint")
         {
-            //Destroy(collision.gameObject);
-            DemoCard.SetActive(true);
+            BrownCard.SetActive(true);
+        }
+        if (collision.gameObject.tag == "waypoint1")
+        {
+            BlueCard.SetActive(true);
+        }
+        if (collision.gameObject.tag == "waypoint2")
+        {
+            PinkCard.SetActive(true);
+        }
+        if (collision.gameObject.tag == "waypoint3")
+        {
+            OrangeCard.SetActive(true);
+        }
+        if (collision.gameObject.tag == "waypoint4")
+        {
+            RedCard.SetActive(true);
+        }
+        if (collision.gameObject.tag == "waypoint5")
+        {
+            YellowCard.SetActive(true);
+        }
+        /*if (collision.gameObject.tag == "waypoint6")
+        {
+            DemoCard.SetActive(false);
+        }*/
+        if (collision.gameObject.tag == "waypoint7")
+        {
+            PurpleCard.SetActive(true);
         }
     }
 
@@ -31,7 +72,46 @@ public class CardControl : MonoBehaviour {
     {
         if (collision.gameObject.tag == "waypoint")
         {
+            BrownCard.SetActive(false);
+        }
+        if (collision.gameObject.tag == "waypoint1")
+        {
+            BlueCard.SetActive(false);
+        }
+        if (collision.gameObject.tag == "waypoint2")
+        {
+            PinkCard.SetActive(false);
+        }
+        if (collision.gameObject.tag == "waypoint3")
+        {
+            OrangeCard.SetActive(false);
+        }
+        if (collision.gameObject.tag == "waypoint4")
+        {
+            RedCard.SetActive(false);
+        }
+        if (collision.gameObject.tag == "waypoint5")
+        {
+            YellowCard.SetActive(false);
+        }
+        /*if (collision.gameObject.tag == "waypoint6")
+        {
             DemoCard.SetActive(false);
+        }*/
+        if (collision.gameObject.tag == "waypoint7")
+        {
+            PurpleCard.SetActive(false);
         }
     }
+
+    /*void ButtonClicked()
+    {
+        BrownCard.SetActive(false);
+        BlueCard.SetActive(false);
+        PinkCard.SetActive(false);
+        OrangeCard.SetActive(false);
+        RedCard.SetActive(false);
+        YellowCard.SetActive(false);
+        PurpleCard.SetActive(false);
+    }*/
 }
