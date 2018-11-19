1ef91c64393a592f27f2c546b97b485a03de127f
Tuuli Haapala <t6hatu01@students.oamk.fi>
2018-11-19 10:11:14 +0200

    cardcode

---
+++ ForestEmp/Assets/Scripts/CardControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardControl : MonoBehaviour {
+
+    public GameObject DemoCard;
+
+    // Use this for initialization
+    void Start()
+    {
+        DemoCard.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "waypoint")
+        {
+            //Destroy(collision.gameObject);
+            DemoCard.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "waypoint")
+        {
+            DemoCard.SetActive(false);
+        }
+    }
+}
